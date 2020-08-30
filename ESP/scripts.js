04e749350e8ee65519b452c89d633041743a0839
jarquafelmu <thechaoselement@gmail.com>
2020-08-29 18:38:01 -0600

    lots of changes

--- ESP/scripts.js
+++ ESP/scripts.js
@@ -1,59 +1,62 @@
-var cards = [{
-        name: "star",
-        style: ''
-    },
-    {
-        name: "square",
-        style: ''
-    },
-    {
-        name: "circle",
-        style: ''
-    },
-    {
-        name: "waves",
-        style: ''
-    },
-    {
-        name: "plus",
-        style: ''
-    }
-];
-var voiceLines = {
-    opening: "I'm going to test you for extra sensory power. The other side of this card is a circle, plus, waves, square, or star. Clear your mind. When you're ready, if you are ever ready, say the name out loud.",
-    correct: "You got that correct but you're still just a mortal.",
-    incorrect: "You were wrong, better luck next time, non-ESP-gifted-mortal.",
-    notvalid: "That was not a valid card. Try again, mortal.",
-    exit: "Goodbye.",
-    next: "Next",
-    console: "That's alright mortal, we can't all be gifted."
-}
-
 var datastore = {
-    recognizing: false,
-    final_transcript: "",
     button_states: {
         start: 'Listen',
         during: 'Listening',
         error: 'Error'
     },
+    cards: [{
+        name: "star",
+        face: '🂱'
+    },
+    {
+        name: "square",
+        face: '🂲'
+    },
+    {
+        name: "circle",
+        face: '🂳'
+    },
+    {
+        name: "waves",
+        face: '🂴'
+    },
+    {
+        name: "plus",
+        face: '🂵'
+    }],
+    mic_img: '🎤',
+    deck: [],
+    decksize: 25,  
     error: {
         speech: 'no-speech',
         mic: 'audio-capture',
         blocked: 'not-allowed'
+    },
+    numcopies: 5,
+    recognizing: false,
+    voicelines: {
+        opening: "I'm going to test you for extra sensory power. The other side of this card is a circle, plus, waves, square, or star. Clear your mind. When you're ready, if you are ever ready, say the name out loud. You may also say 'cancel' if you are scared.",
+        // correct: "You got that correct but you're still just a mortal.",
+        correct: "correct",
+        incorrect: "You were wrong, better luck next time, non-ESP-gifted-mortal.",
+        notvalid: "invalid",
+        notunderstood: "I didn't get that, try again",
+        exit: "Goodbye.",
+        next: "Next",
+        console: "That's alright mortal, we can't all be gifted."
     }
 }
 $("#start_button").text(datastore.button_states.start);
 
 var recognition = null;
-{
+{ // voice recognition setup
     var diagnostic = $("#diagnostic");
 
     var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
     var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList;
 
     recognition = new SpeechRecognition();
-    var grammar = '#JSFG V1.0; grammar cards; public <card> = star | circle | square | wave | waves | plus ;'
+    var grammar = '#JSFG V1.0; grammar cards; public <card> = star | circle | square | wave | waves | plus | cancel | quit ;'
     var speechRecognitionList = new SpeechGrammarList();
     speechRecognitionList.addFromString(grammar, 1);
 
@@ -64,65 +67,60 @@ var recognition = null;
     recognition.maxAlternatives = 1;
     var recognizing = false;
 
-    recognition.addEventListener('start', function(){
+    recognition.addEventListener('start', function () {
         datastore.recognizing = true;
-        console.log('started listening');
+        console.debug('started listening');
         setSpeechButtonText(datastore.button_states.during);
+        $("#overlay").show();
     });
 
-    recognition.addEventListener('result', function(){
+    recognition.addEventListener('result', function () {
         var result = event.results[0][0];
         let word = result.transcript.toLowerCase();
-        diagnostic.text(`Result received ${word}`);
-        console.log(`Result received ${word}`);
-        console.log(`Confidence: ${result.confidence}`);
+        console.debug(`Result received ${word}`);
+
         const card = getCard(word);
         if (card === undefined) {
-            console.log("not a valid card");
-            speak(voiceLines.notvalid);
+            console.debug("not a valid card");
+            speak(datastore.voicelines.notvalid);
         } else {
-            console.log("valid card");
-            speak(voiceLines.correct);
+            console.debug("valid card");
+            speak(datastore.voicelines.correct);
+            hideCards(true);
+            $(`#${card.name}`).show();
+            
         }
     });
 
-    recognition.addEventListener('end', function(){
+    recognition.addEventListener('end', function () {
         datastore.recognizing = false;
-        console.log('stopped listening');
+        console.debug('stopped listening');
         setSpeechButtonText(datastore.button_states.start);
+        $("#overlay").hide();
     });
 
-    recognition.onspeechend = function(){
-        console.log("Speech has stopped being detected");
+    recognition.onspeechend = function () {
+        console.debug("Speech has stopped being detected");
     }
 
-    recognition.onnomatch = function() {
+    recognition.onnomatch = function () {
         diagnostic.text("I didn't recognize that word");
-        console.log("Speech not recognized");
+        console.debug("Speech not recognized");
     }
 
-    recognition.onerror = function(event) {
-        diagnostic.text(`Error occured in recognition: ${event.error}`);
+    recognition.onerror = function (event) {
+        console.error(`error: ${event.error}`);
     }
 }
 
-
-$('#speak').click(() => {
-    if (datastore.recognizing) return;
-    setSpeechButtonText(datastore.button_states.during);
-    recognition.start();
-    console.log('Ready to recieve a color command');
-});
-
-function getCard(input){
-    return cards.find(element => element.name === input);
+function getCard(input) {
+    return datastore.cards.find(element => element.name === input);
 }
 
 function setSpeechButtonText(text) {
-    console.log(`setting button text to: ${text}`);
+    // console.log(`setting button text to: ${text}`);
     $("#speak").html(text);
 }
-
 
 function speak(msg) {
     if (!('speechSynthesis' in window)) {
@@ -136,14 +134,61 @@ function speak(msg) {
     }
 }
 
+function hideCards(all = false) {
+    let target = ".card"
+    if (!all)
+        target = `${target}.face`
+    $(target).hide();
+}
+
+
 function begin() {
-    speak(voiceLines.opening);
+    speak(datastore.voicelines.opening);
 }
 
 function correct() {
-    speak(voiceLines.correct);
+    speak(datastore.voicelines.correct);
 }
 
 function incorrect() {
-    speak(voiceLines.incorrect);
+    speak(datastore.voicelines.incorrect);
 }
+
+function buildDeck() {
+    datastore.cards.forEach((card) => {
+        for (let i = 0; i < datastore.numcopies; ++i) {
+            deck.push(card.face);
+        }
+    });
+}
+
+function deal() {
+    return deck[Math.random * datastore.decksize];
+}
+
+hideCards();
+
+$('#speak').click(() => {
+    if (datastore.recognizing) return;
+    recognition.start();
+    setSpeechButtonText(datastore.button_states.during);
+    console.log('Ready to receive a color command');
+});
+
+// $("#overlay").click(() => {
+//     $(this).hide();
+//     recognition.stop();
+// });
+
+// game
+function game() {
+    // pick a card
+    // introduce the user
+    // user guesses
+    // give feedback
+    // if correct mark and next
+    // if incorrect 'console' and next
+    // loop 25 times
+}
+
+
