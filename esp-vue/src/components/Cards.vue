515a55d1095de11834a4c2a63e70998f94e8cba3
jarquafelmu <thechaoselement@gmail.com>
2020-10-04 14:44:06 -0600

    showing cards

--- esp-vue/src/components/Cards.vue
+++ esp-vue/src/components/Cards.vue
@@ -1,33 +1,55 @@
 <template>
   <div>
-    <div class="border rounded border-primary h-100 py-3 px-1">
-      <span id="card-body" class=""
-        ><img src="svg/square.svg" alt="a square card" srcset=""
-      /></span>
-    </div>
-    <div class="border rounded border-primary h-100 py-3 px-1">
-      <img src="svg/circle.svg" alt="a circle card" srcset="" />
-      <div id="card-body" class="align-self-center"></div>
-    </div>
-    <div class="col border rounded border-primary h-100 py-3 px-1">
-      <div id="card-body">
-        <img src="svg/star.svg" alt="a star card" srcset="" />
+    <div class="border rounded border-primary zener-card">
+      <div class="card-body">
+        <img
+          class="card-img"
+          src="../assets/cards/square.svg"
+          alt="a square card"
+        />
       </div>
     </div>
-    <div class="col border rounded border-primary h-100 py-3 px-1">
-      <div id="card-body">
-        <img src="svg/waves.svg" alt="a waves card" srcset="" />
+    <div class="border rounded border-primary zener-card">
+      <div class="card-body">
+        <img
+          class="card-img"
+          src="../assets/cards/circle.svg"
+          alt="a circle card"
+        />
       </div>
     </div>
-    <div class="col border rounded border-primary h-100 py-3 px-1">
-      <div id="card-body">
-        <img src="svg/plus.svg" alt="a plus card" srcset="" />
+    <div class="border rounded border-primary zener-card">
+      <div id="card-body" class="card-body">
+        <img
+          class="card-img"
+          src="../assets/cards/star.svg"
+          alt="a star card"
+        />
       </div>
     </div>
+    <div class="border rounded border-primary zener-card">
+      <div class="card-body">
+        <img
+          class="card-img"
+          src="../assets/cards/waves.svg"
+          alt="a waves card"
+        />
+      </div>
+    </div>
+    <div class="border rounded border-primary zener-card">
+      <div class="card-body">
+        <img
+          class="card-img"
+          src="../assets/cards/plus.svg"
+          alt="a plus card"
+        />
+      </div>
+    </div>it was 
   </div>
 </template>
 <script>
 export default {
+  props: ["cards"],
   name: "Cards",
 };
 </script>
