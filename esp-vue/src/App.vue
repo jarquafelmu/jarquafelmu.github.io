88091f75e13b522092057f7237b78185281b474b
jarquafelmu <thechaoselement@gmail.com>
2020-10-07 06:57:06 -0600

    stubbing out methods

--- esp-vue/src/App.vue
+++ esp-vue/src/App.vue
@@ -25,11 +25,48 @@ export default {
         scheme: null,
         motion: null,
       },
+      darkThemePath: "css/darkster.css",
+      devicePreferencesTags: {
+        motion: "",
+        scheme: ""
+      }
     };
   },
   components: {
     Manager,
   },
+  methods: {
+    schemeListener: function() {
+      // TODO: get scheme selector state from Manager
+    },
+    applyDarkTheme: function() {
+      let file = document.createElement('link');
+      file.rel = 'stylesheet';
+      file.href = this.darkThemePath;
+      document.head.appendChild(file);
+    },
+    removeDarkTheme: function() {
+      // TODO: figure out how to target a specific stylesheet for removal
+    },
+    storeSchemeStateInCookie: function() {
+      // TODO: store the scheme state in cookie
+    },
+    getSchemeStateFromCookie: function() {
+      // TODO: get the scheme state from cookie
+    },
+    storeMotionInCookie: function() {
+      // TODO: store the user's vibration option from cookie
+    },
+    getMotionFromCookie: function() {
+      // TODO: get the user's motion option from cookie
+    },
+    getDevicePreference: function(preference){
+      // TODO: get the preference from the user's device
+    },
+    subscribeDevicePreference: function(event) {
+      // TODO: listen for changes with the given preference
+    }
+  }
 };
 </script>
 
