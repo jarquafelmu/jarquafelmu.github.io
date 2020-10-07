bea94d38d36af5f265d88a922212e055ee7693ec
Sam <thechaoselement@gmail.com>
2020-10-07 16:13:33 -0600

    creating a modal for user settings

---
+++ esp-vue/src/components/SettingsModal.vue
@@ -0,0 +1,16 @@
+<template id="settingsModal">
+  <div>
+    <button id="hiddenModalBtn w-0 h-0" class="d-none"></button>
+  </div>
+</template>
+<script>
+export default {
+  name: "SettingsModal",
+};
+</script>
+<style lang="scss">
+// #hiddenModalBtn {
+//   width: 0;
+//   height: 9;
+// }
+</style>
