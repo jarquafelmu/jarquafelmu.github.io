a7d4c63a774a8647970325545b9b181025480821
jarquafelmu <thechaoselement@gmail.com>
2020-10-07 22:33:48 -0600

    settings cog wheeel

---
+++ esp-vue/src/components/SettingsBtn.vue
@@ -0,0 +1,93 @@
+<template>
+  <div
+    id="SettingsBtn"
+    :title="title"
+    class="form-check form-switch sticky-top"
+    @click="handle"
+  >
+    <svg
+      id="SettingsBtnGlyph"
+      :title="title"
+      viewBox="0 0 16 16"
+      class="bi bi-gear-wide-connected"
+      :class="{ 'text-success': !active, 'text-primary': active }"
+      fill="currentColor"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fill-rule="evenodd"
+        d="M8.932.727c-.243-.97-1.62-.97-1.864 0l-.071.286a.96.96 0 0 1-1.622.434l-.205-.211c-.695-.719-1.888-.03-1.613.931l.08.284a.96.96 0 0 1-1.186 1.187l-.284-.081c-.96-.275-1.65.918-.931 1.613l.211.205a.96.96 0 0 1-.434 1.622l-.286.071c-.97.243-.97 1.62 0 1.864l.286.071a.96.96 0 0 1 .434 1.622l-.211.205c-.719.695-.03 1.888.931 1.613l.284-.08a.96.96 0 0 1 1.187 1.187l-.081.283c-.275.96.918 1.65 1.613.931l.205-.211a.96.96 0 0 1 1.622.434l.071.286c.243.97 1.62.97 1.864 0l.071-.286a.96.96 0 0 1 1.622-.434l.205.211c.695.719 1.888.03 1.613-.931l-.08-.284a.96.96 0 0 1 1.187-1.187l.283.081c.96.275 1.65-.918.931-1.613l-.211-.205a.96.96 0 0 1 .434-1.622l.286-.071c.97-.243.97-1.62 0-1.864l-.286-.071a.96.96 0 0 1-.434-1.622l.211-.205c.719-.695.03-1.888-.931-1.613l-.284.08a.96.96 0 0 1-1.187-1.186l.081-.284c.275-.96-.918-1.65-1.613-.931l-.205.211a.96.96 0 0 1-1.622-.434L8.932.727zM8 12.997a4.998 4.998 0 1 0 0-9.995 4.998 4.998 0 0 0 0 9.996z"
+      />
+      <path
+        fill-rule="evenodd"
+        d="M7.375 8L4.602 4.302l.8-.6L8.25 7.5h4.748v1H8.25L5.4 12.298l-.8-.6L7.376 8z"
+      />
+    </svg>
+    <button
+      id="toggleSettingsModal"
+      type="button"
+      data-toggle="modal"
+      data-target="#SettingsModalId"
+      class="btn d-none w-0 h-0"
+    ></button>
+  </div>
+</template>
+
+<script>
+import { bus } from "../main";
+export default {
+  name: "SettingsBtn",
+  data() {
+    return {
+      schemeCheckState: false,
+      currentScheme: "Dark",
+      title: "Settings",
+      active: false,
+    };
+  },
+  methods: {
+    seeChange: function () {
+      this.active = !this.active;
+      console.log("in 'seeChange'");
+      // document.querySelector("#toggleSettingsModal").click();
+    },
+    schemeChangeHandler: function () {
+      this.schemeCheckState = document.querySelector(
+        `#SettingsBtnSwitch`
+      ).checked;
+      console.log("schemeCheckState:", this.schemeCheckState);
+      bus.$emit("schemeChoiceChanged", this.schemeCheckState);
+      console.log(`sent schemeChoiceChanged event`);
+    },
+    handle: function () {
+      console.log("in 'handle'");
+      bus.$emit("showModal", true);
+      console.log("sent 'showModal' broadcast");
+      // /* const target =  */ document.getElementById("toggleSettingsModal");
+      // console.dir(target);
+      // target.click();
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+#SettingsBtn {
+  position: absolute;
+  right: 25%;
+  top: 3%;
+  font-size: 2rem;
+  opacity: 30%;
+  transition: 0.18s ease;
+
+  #SettingsBtnGlyph {
+    width: 1em;
+    height: 1em;
+  }
+
+  &:hover {
+    opacity: 1;
+    cursor: pointer;
+  }
+}
+</style>
