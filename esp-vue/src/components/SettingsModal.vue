7ea770a2a1aaee7682c506f2ab03689492e83013
jarquafelmu <thechaoselement@gmail.com>
2020-10-07 22:34:12 -0600

    modal for providing options

--- esp-vue/src/components/SettingsModal.vue
+++ esp-vue/src/components/SettingsModal.vue
@@ -1,16 +1,69 @@
-<template id="settingsModal">
-  <div>
-    <button id="hiddenModalBtn w-0 h-0" class="d-none"></button>
+<template>
+  <div
+    id="SettingsModalId"
+    class="modal fade"
+    tabIndex="-1"
+    aria-labelledby="SettingsModalLabel"
+    aria-hidden="true"
+  >
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <div class="modal-title">Settings</div>
+        </div>
+        <div class="modal-body">
+          <div class="container">
+            <div class="row">
+              <div class="col">
+                <SchemeToggle />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-primary" @click="toggleModal">
+            Save
+          </button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
+import { bus, bootstrap } from "../main";
+import SchemeToggle from "./SchemeToggle";
+// import bootstrap from "~Bootstrap";
+
 export default {
   name: "SettingsModal",
+  data() {
+    return {
+      modal: null,
+    };
+  },
+  created: function () {
+    this.modal = new bootstrap.Modal(
+      document.getElementById("SettingsModalId")
+    );
+    console.dir(`stored modal:`, this.modal);
+    this.registerShowModal();
+  },
+  components: {
+    SchemeToggle,
+  },
+  methods: {
+    toggleModal: function () {
+      // document.querySelector("#toggleSettingsModal").click();
+    },
+    registerShowModal: function () {
+      const that = this;
+      bus.$on("showModal", () => {
+        console.log("request received to show modal");
+        that.modal.show();
+      });
+    },
+  },
 };
 </script>
 <style lang="scss">
-// #hiddenModalBtn {
-//   width: 0;
-//   height: 9;
-// }
 </style>
