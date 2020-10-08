dcd99ce5d491aeb6bd894bf1e602b66634c21c52
jarquafelmu <thechaoselement@gmail.com>
2020-10-07 22:34:55 -0600

    creating bus and bootstrap

--- esp-vue/src/main.js
+++ esp-vue/src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue';
+import bs from 'bootstrap/dist/js/bootstrap.bundle';
 
 // import 'bootstrap/dist/css/bootstrap.css';
 // import 'bootstrap-vue/dist/bootstrap-vue.css';
@@ -7,6 +8,8 @@ import App from './App.vue';
 import './styles.scss'
 
 Vue.config.productionTip = false
+export const bus = new Vue();
+export const bootstrap = bs;
 
 new Vue({
   render: h => h(App),
